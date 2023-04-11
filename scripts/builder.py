4043e9da32804e8096c343f6043bf557f89045d9
Ivan Begtin <ibegtin@gmail.com>
2023-04-11 16:40:42 +0300

    Updated country and owner info of multiple catalogues

--- scripts/builder.py
+++ scripts/builder.py
@@ -2,6 +2,13 @@
 # This script intended to enrich data of catalogs entries 
 
 import typer
+import datetime
+import requests
+from requests.exceptions import ConnectionError
+from urllib3.exceptions import InsecureRequestWarning
+# Suppress only the single warning from urllib3 needed.
+requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+
 import yaml
 try:
     from yaml import CLoader as Loader, CDumper as Dumper
@@ -52,6 +59,7 @@ def build():
 
 @app.command()
 def report():
+    """Report incomplete data per set"""
     data = load_jsonl(os.path.join(DATASETS_DIR, 'catalogs.jsonl'))
     typer.echo('')
     for d in data:     
@@ -76,6 +84,7 @@ def report():
 
 @app.command()
 def validate():
+    """Validates YAML entities files against simple Cerberus schema"""
     from cerberus import Validator
     schema_file = os.path.join(DATASETS_DIR, '../schemes/catalog.json')
     f = open(schema_file, 'r', encoding='utf8')
@@ -95,6 +104,103 @@ def validate():
                 print('%s error %s' % (d['id'], str(e)))
 
 
+headers = {'Accept': 'application/json'}
+
+@app.command()
+def statusreport(updatedata=False):
+    """Checks every site existence and endpoints availability except Geonetwork yet"""    
+    session = requests.Session()
+    session.max_redirects = 100    
+    results = []
+    records = load_jsonl(os.path.join(DATASETS_DIR, 'catalogs.jsonl'))
+    typer.echo('Loaded %d data catalog records' % (len(records)))
+    out = open('statusreport.jsonl', 'w', encoding='utf8')    
+    for item in records:
+
+        report = {'id' : item['id'], 'name' : item['name'] if 'name' in item.keys() else '', 'link' : item['link'], 'date_verified' : datetime.datetime.now().isoformat(), 'software' : item['software'] if 'software' in item.keys() else ''}
+        report['api_status'] = 'uncertain'
+        report['api_http_code'] = ''
+        report['api_content_type'] = ''
+        try:
+            response = session.head(item['link'], verify=False, allow_redirects=True)
+        except ConnectionError as e:
+            report['status'] = 'deprecated'
+            report['link_http_code'] = ''
+            print(report)
+            out.write(json.dumps(report) + '\n')
+            continue            
+
+        report['link_http_code'] = response.status_code                
+        if not response.ok:
+            report['status'] = 'deprecated'
+            report['api_status'] = 'deprecated'
+        else:
+            report['status'] = 'active'
+        if 'software' in item.keys():
+            if item['software'] == 'CKAN':
+                if 'endpoints' in item.keys() and len(item['endpoints']) > 0:
+                    if item['endpoints'][0]['type'] == 'ckanapi':
+                        search_endpoint = item['endpoints'][0]['url'] + '/action/package_search'
+                        response = session.get(search_endpoint, headers=headers, verify=False)
+                        report['api_http_code'] = response.status_code
+                        report['api_content_type'] = response.headers['content-type']
+                        if not response.ok:
+                            report['api_status'] = 'deprecated'
+                        else:
+                            if response.headers['content-type'].split(';',1)[0].lower() == 'application/json':
+                                report['api_status'] = 'active'
+            elif item['software'] == 'NADA':
+               if 'endpoints' in item.keys() and len(item['endpoints']) > 0:
+                    if item['endpoints'][0]['type'] == 'nada:catalog-search':                
+                        search_endpoint = item['endpoints'][0]['url']
+                        response = session.get(search_endpoint, headers=headers, verify=False)
+                        report['api_http_code'] = response.status_code
+                        report['api_content_type'] = response.headers['content-type']
+                        if not response.ok:
+                            report['api_status'] = 'deprecated'
+                        else:
+                            if response.headers['content-type'].split(';',1)[0].lower() == 'application/json':
+                                report['api_status'] = 'active'
+            elif item['software'] == 'Dataverse':
+               if 'endpoints' in item.keys() and len(item['endpoints']) > 0:
+                    if item['endpoints'][0]['type'] == 'dataverseapi':                
+                        search_endpoint = item['endpoints'][0]['url']
+                        response = session.get(search_endpoint, headers=headers, verify=False)
+                        report['api_http_code'] = response.status_code
+                        report['api_content_type'] = response.headers['content-type']
+                        if not response.ok:
+                            report['api_status'] = 'deprecated'
+                        else:
+                            if response.headers['content-type'].split(';',1)[0].lower() == 'application/json':
+                                report['api_status'] = 'active'
+            elif item['software'] == 'InvenioRDM':
+               if 'endpoints' in item.keys() and len(item['endpoints']) > 0:
+                    endpoints = {item['type']:item for item in item['endpoints']}
+                    if 'inveniordmapi:records' in endpoints.keys():
+                        search_endpoint = endpoints['inveniordmapi:records']['url']
+                        response = session.get(search_endpoint, headers=headers, verify=False)
+                        report['api_http_code'] = response.status_code
+                        report['api_content_type'] = response.headers['content-type']
+                        if not response.ok:
+                            report['api_status'] = 'deprecated'
+                        else:
+                            if response.headers['content-type'].split(';',1)[0].lower() == 'application/json':
+                                report['api_status'] = 'active'
+            elif item['software'] == 'uData':
+                search_endpoint = item['link'] + '/api/1/datasets/'
+                response = session.get(search_endpoint, headers=headers, verify=False)
+                report['api_http_code'] = response.status_code
+                report['api_content_type'] = response.headers['content-type']
+                if not response.ok:
+                    report['api_status'] = 'deprecated'
+                else:
+                    if response.headers['content-type'].split(';',1)[0].lower() == 'application/json':
+                        report['api_status'] = 'active'
+
+        print(report)
+        out.write(json.dumps(report) + '\n')
+
+            
 
 if __name__ == "__main__":    
     app()
