03907524d78854e1eebfac93fc69965bc7cb7890
Ivan Begtin <ibegtin@gmail.com>
2023-05-24 22:56:00 +0300

    Added cross references to wikidata

--- scripts/identifiers.sh
+++ scripts/identifiers.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-python enrich.py  enrich-identifiers ../../commondata-crawl/datasources/re3data/re3data.tsv re3data
-python enrich.py  enrich-identifiers ../../commondata-crawl/datasources/roar/eprints.tsv roar
-python enrich.py  enrich-identifiers ../../commondata-crawl/datasources/dataportals/mapped.tsv dataportals.org
+#python enrich.py  enrich-identifiers ../../commondata-crawl/datasources/re3data/re3data.tsv re3data
+#python enrich.py  enrich-identifiers ../../commondata-crawl/datasources/roar/eprints.tsv roar
+#python enrich.py  enrich-identifiers ../../commondata-crawl/datasources/dataportals/mapped.tsv dataportals.org
+python enrich.py  enrich-identifiers ../../commondata-crawl/datasources/wikidata/wikidata.tsv wikidata
