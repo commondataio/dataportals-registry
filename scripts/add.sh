de30d607a96a955dce18dcfd776d5d26cf41d69e
Ivan Begtin <ibegtin@gmail.com>
2023-08-20 08:56:39 +0300

    Added hundreds of new data catalogs

---
+++ scripts/add.sh
@@ -0,0 +1,27 @@
+python builder.py add-list --software arcgishub arcgishub.txt
+python builder.py add-list --software arcgisserver arcgishubserver.txt
+python builder.py add-list --software aleph aleph.txt
+python builder.py add-list --software ckan ckan.txt
+python builder.py add-list --software dkan dkan.txt
+python builder.py add-list --software drupal drupal.txt
+python builder.py add-list --software custom custom.txt
+python builder.py add-list --software custom --catalog-type "Datasets list" datasetslist.txt
+python builder.py add-list --software dspace dspace.txt
+python builder.py add-list --software eprints eprints.txt
+python builder.py add-list --software dataverse dataverse.txt
+python builder.py add-list --software geonetwork geonetwork.txt
+python builder.py add-list --software geonode geonode.txt
+python builder.py add-list --software geoserver geoserver.txt
+python builder.py add-list --software custom --catalog-type "Geoportal" geographic.txt
+python builder.py add-list --software custom --catalog-type "Machine learning catalog" ml.txt
+python builder.py add-list --software custom --catalog-type "Indicators catalog" indicators.txt
+python builder.py add-list --software custom --catalog-type "Data search engine" search.txt
+python builder.py add-list --software ipt ipt.txt
+python builder.py add-list --software opendatasoft opendatasoft.txt
+python builder.py add-list --software pxweb pxweb.txt
+python builder.py add-list --software custom --catalog-type "Scientific data repository" scientific.txt
+python builder.py add-list --software socrata socrata.txt
+python builder.py add-list --software thredds thredds.txt
+python builder.py add-list --software erddap erddap.txt
+python builder.py add-list --software wordpress wordpress.txt
+python builder.py add-list --software elsevierdigitalcommons digitalcommons.txt
